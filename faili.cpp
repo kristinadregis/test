2acd537a5ec3749c11304f652f963a066f767ee1
kristinadregis <kristinadregis@gmail.com>
2026-09-23 09:43:38 +0300

    VĀRDS, UZVĀRDS,VECUMS

--- faili.cpp
+++ faili.cpp
@@ -4,7 +4,22 @@ using namespace std;
 
 int main(){
     ofstream fails("dati.txt");
-    fails << "Sveiki!"<<endl;
+    cout<<"Ievadi savu vārdu!"<<endl;
+    string name;
+    cin >> name;
+
+    cout<<"Ievadi savu uzvārdu"<<endl;
+    string lastname;
+    cin >> lastname;
+
+    cout<<"Ievadi savu vecumu"<<endl;
+    int age;
+    cin >> age;
+
+    fails << "Vārds: "<<name<<endl;
+    fails << "Uzvārds: "<<lastname<<endl;
+    fails << "Vecums: "<<age<<endl;
+
     fails.close();
 
       return 0;
